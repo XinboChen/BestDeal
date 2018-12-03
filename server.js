2dd93d4a7666f254e9efd84880aef1689a0e88ed
XinboChen <xinbo.chen@plantronics.com>
2018-12-03 17:53:09 -0600

    fix can't find module 'html' bug

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require("express");
+var engines = require('consolidate');
+
 app = express();
 
-app.use(express.static(__dirname + "/"));
+app.set('views', __dirname + '/app/views');
+app.engine('html', engines.mustache);
+app.set('view engine', 'html'); 
+app.get('*',(req, res)=> {
+    res.render("index.html");
+});
+
 app.listen(8080);
 console.log("Express is listening port 8080");
 
