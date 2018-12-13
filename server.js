a02768f4eeb47b44e7488505abff5b34492a7218
XinboChen <xinbo.chen@plantronics.com>
2018-12-13 16:57:35 -0600

    updating loging micro service

--- server.js
+++ server.js
@@ -1,21 +1,80 @@
-var express = require("express");
-var engines = require('consolidate');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+// [SH] Require Passport
+var passport = require('passport');
 
-app = express();
+// [SH] Bring in the data model
+require("./app/models/db");
+// [SH] Bring in the Passport config after model is defined
+require('passport');
 
-app.set('pages', __dirname + '/app/bestDeal/page/home');
-app.engine('html', engines.mustache);
-app.set('view engine', 'html'); 
-app.get('*',(req, res)=> {
-    res.render("index.html");
+
+// [SH] Bring in the routes for the API (delete the default routes)
+var routesApi = require('./api/routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors());
+
+// [SH] Initialise Passport before using the route middleware
+app.use(passport.initialize());
+
+// [SH] Use the API routes when path starts with /api
+app.use('/api', routesApi);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-app.listen(8080);
-console.log("Express is listening port 8080");
+// error handlers
 
-var users = [
-    {  
-        email: "xinbo@gmail.com",
-        password: "abcde"
-    
-}];
+// [SH] Catch unauthorised errors
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401);
+    res.json({"message" : err.name + ": " + err.message});
+  }
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
