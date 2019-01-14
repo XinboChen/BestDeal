10a503015afe0d32b6a6dc1435f784d3593b4cfe
XinboChen <xinbo.chen@plantronics.com>
2019-01-14 17:24:23 -0600

    npm initialization work for server.js and app.js and package.json

--- server.js
+++ server.js
@@ -1,80 +1,47 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-// [SH] Require Passport
-var passport = require('passport');
+// './app' make sure server.js can find app.js. otherwise will return can't find module app error
+var app = require('./app');
+var debug = require('debug')('BestDeal:server');
+var http = require('http');
 
-// [SH] Bring in the data model
-require("./app/models/db");
-// [SH] Bring in the Passport config after model is defined
-require('passport');
+var port = normalizePort(process.env.PORT ||  '3003');
+app.set('port', port);
 
+var server = http.createServer(app);
 
-// [SH] Bring in the routes for the API (delete the default routes)
-var routesApi = require('./api/routes/index');
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(cors());
-
-// [SH] Initialise Passport before using the route middleware
-app.use(passport.initialize());
-
-// [SH] Use the API routes when path starts with /api
-app.use('/api', routesApi);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// [SH] Catch unauthorised errors
-app.use(function (err, req, res, next) {
-  if (err.name === 'UnauthorizedError') {
-    res.status(401);
-    res.json({"message" : err.name + ": " + err.message});
-  }
-});
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+function normalizePort(val){
+    var port = parseInt(val, 10);
+    
+    if(isNaN(port)){
+       // pipe
+        return val;
+    }
+    
+    if(port >= 0){
+        return port;
+    }
+    
+    return false;
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+function onError(error){
+    if(error.syscall != 'listen'){
+        throw error;
+    }
+    
+    var bind = typeof port === 'string' 
+        ? 'Pipe' + port 
+        : 'Port' + port;
+}
 
+function onListening(){
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+        ? 'pipe' + addr
+        : 'port' + addr.port;
+    debug('Listening on' + bind);
+}
 
-module.exports = app;
